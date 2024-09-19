7d70f727758564a514fb4ee3205ee012fbf1fe4f
mike <greenemcg@gmail.com>
2024-09-19 09:02:10 -0600

    Adding files

---
+++ src/main/java/nm/poolio/views/transaction/UserPoolioTransactionView.java
@@ -0,0 +1,16 @@
+package nm.poolio.views.transaction;
+
+
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import jakarta.annotation.security.RolesAllowed;
+import lombok.extern.slf4j.Slf4j;
+import nm.poolio.views.MainLayout;
+
+@PageTitle("Transactions \uD83D\uDCB8")
+@Route(value = "userTransactions", layout = MainLayout.class)
+@RolesAllowed("ADMIN")
+@Slf4j
+public class UserPoolioTransactionView {
+
+}
